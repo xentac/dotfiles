1f83f66668de303b4cb44976e02bd41d55f8583c
Jason Chu <jchu@xentac.net>
2020-06-04 10:27:34 -0700

    Add inputrc

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
+ln -s $(pwd)/inputrc $HOME/.inputrc
+
 rm -rf $HOME/.tmux
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm && $HOME/.tmux/plugins/tpm/bin/install_plugins
