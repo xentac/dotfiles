acc99e3e8ff31bc4ada1cea674c0e1e6eac55cd2
Jason Chu <jchu@xentac.net>
2020-11-09 15:39:36 -0800

    Add I3 config to dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+mkdir -p $HOME/.config/i3
+rm $HOME/.config/i3/config
+ln -s $(pwd)/i3-config $HOME/.config/i3/config
+
+rm $HOME/.i3blocks.conf
+ln -s $(pwd)/i3blocks.conf $HOME/.i3blocks.conf
+
+rm $HOME/.tmux.conf
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
 rm -rf $HOME/.tmux
