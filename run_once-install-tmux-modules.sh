6fa03e63b129a73ab8bf27196cde81e0e819a94c
Jason Chu <jchu@xentac.net>
2021-12-26 17:40:48 -0800

    Run install and update for tpm

--- run_once-install-tmux-modules.sh
+++ run_once-install-tmux-modules.sh
@@ -4,8 +4,8 @@ TPM_DIR="$HOME/.tmux/plugins/tpm"
 
 if [ ! -d "$TPM_DIR" ]; then
   git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
-  "$TPM_DIR"/bin/install_plugins
 else
   cd "$TPM_DIR" && git pull --ff-only
-  "$TPM_DIR"/bin/update_plugins all
 fi
+"$TPM_DIR"/bin/install_plugins
+"$TPM_DIR"/bin/update_plugins all
