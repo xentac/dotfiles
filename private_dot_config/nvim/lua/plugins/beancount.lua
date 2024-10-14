5e16123f35c0ceaaf300465e02e9fb58982a07ab
Jason Chu <jchu@xentac.net>
2024-10-14 07:58:21 -1000

    Don't force beancount filetype because neovim is too slow with it

--- private_dot_config/nvim/lua/plugins/beancount.lua
+++ private_dot_config/nvim/lua/plugins/beancount.lua
@@ -1,11 +1,4 @@
 return {
   { "jmcantrell/vim-virtualenv", version = false },
-  {
-    "nathangrigg/vim-beancount",
-    version = false,
-    ft = "beancount",
-    config = function()
-      vim.o.foldlevel = 0
-    end,
-  },
+  { "nathangrigg/vim-beancount", version = false },
 }
