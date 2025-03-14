ed8801355e745ca0745b98c4489c1754ab616908
Jason Chu <jchu@xentac.net>
2025-03-14 07:30:56 -1000

    All kitty-scrollback support

---
+++ private_dot_config/nvim/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,19 @@
+return {
+  -- add gruvbox
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = true,
+    lazy = false,
+    cmd = {
+      "KittyScrollbackGenerateKittens",
+      "KittyScrollbackCheckHealth",
+      "KittyScrollbackGenerateCommandLineEditing",
+    },
+    event = { "User KittyScrollbackLaunch" },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^3.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require("kitty-scrollback").setup()
+    end,
+  },
+}
