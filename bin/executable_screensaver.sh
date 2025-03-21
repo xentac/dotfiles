eeca50fce6e3d6bb53fae8b4874de7fa7da83cdb
Jason Chu <jchu@xentac.net>
2025-03-21 08:48:53 -1000

    Modify screensaver script to have different timers for ac vs battery

--- bin/executable_screensaver.sh
+++ bin/executable_screensaver.sh
@@ -3,23 +3,43 @@
 # Only exported variables can be used within the timer's command.
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
-# Run xidlehook
-xidlehook \
-  `# Reset timers after sleep` \
-  --detect-sleep \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 600 seconds, undim if user becomes active` \
-  --timer 600 \
+run_idlehook() {
+  killall xidlehook
+
+  timer=600
+  if [ "$1" = "AC" ]; then
+    timer=3600
+  fi
+  # Run xidlehook
+  xidlehook \
+    `# Reset timers after sleep` \
+    --detect-sleep \
+    `# Don't lock when there's a fullscreen application` \
+    --not-when-fullscreen \
+    `# Don't lock when there's audio playing` \
+    --not-when-audio \
+    `# Dim the screen after 600 seconds, undim if user becomes active` \
+    --timer $timer \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 60 more seconds` \
-  --timer 60 \
+    `# Undim & lock after 60 more seconds` \
+    --timer 60 \
     'setxkbmap -model pc104 -layout us,us -variant dvorak, -option -option grp:alts_toggle; xmodmap ~/.Xmodmap; xrandr --output "$PRIMARY_DISPLAY" --brightness 1; i3lock -c 002233' \
     '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
-   'systemctl suspend' \
-   ''
+    `# Finally, suspend an hour after it locks` \
+    --timer 3600 \
+    'systemctl suspend' \
+    '' &
+}
+
+acpi_listen | while read event; do
+  if [[ $event == *"ac_adapter"* ]]; then
+    if [[ $event == *"00000001" ]]; then
+      echo "AC Plugged In"
+      run_idlehook "AC"
+    else
+      echo "AC Unplugged"
+      run_idlehook "battery"
+    fi
+  fi
+done
