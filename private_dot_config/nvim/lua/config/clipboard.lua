254008a9fedb5ca00629ef827ba7ec31b89649cc
Jason Chu <jchu@xentac.net>
2026-07-31 08:14:18 -0700

    Support clipboard copies through claude and tmux

--- private_dot_config/nvim/lua/config/clipboard.lua
+++ private_dot_config/nvim/lua/config/clipboard.lua
@@ -1,17 +1,17 @@
 -- Use system clipboard for all yank/delete/put operations
 vim.opt.clipboard = "unnamedplus"
 
--- (Optional) If you are using Neovim over SSH, you can force OSC 52 integration:
-if vim.fn.getenv("SSH_TTY") ~= vim.NIL then
+-- Force OSC 52 integration if running over SSH or inside TMUX
+if vim.env.SSH_TTY or vim.env.TMUX then
   vim.g.clipboard = {
     name = "osc52",
     copy = {
-      ["+"] = require("vim.ui.osc52").copy,
-      ["*"] = require("vim.ui.osc52").copy,
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
     paste = {
-      ["+"] = require("vim.ui.osc52").paste,
-      ["*"] = require("vim.ui.osc52").paste,
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
     },
   }
 end
